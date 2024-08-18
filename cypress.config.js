8c4601c01ea09db21703b0a8191392e98476ad74
TGBAKC <132302938+TGBAKC@users.noreply.github.com>
2024-08-18 11:31:52 +0200

    readme file added

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,6 @@ export default defineConfig({
     setupNodeEvents() {
       // implement node event listeners here
     },
+    supportFile: "cypress/support/e2e.js", // Support dosyasını belirtin
   },
 });
